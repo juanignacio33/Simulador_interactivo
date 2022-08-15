e4f7e1e907525fc4134099ced7f185a5cbe87fad
juan benitez <juanignacioybr@gmail.com>
2022-08-15 19:21:51 -0300

    reentrega3zz

--- js/desafioUno.js
+++ js/desafioUno.js
@@ -1,9 +1,20 @@
 let nombre = prompt("Buenos Dias! estas de suerte , pero de la Buena! cual es tu nombre? ")
-    alert("bienvenido " + nombre + "!!!")
-    console.log(" un credito de cuanto quiere conseguir ?maximo es $300,000", nombre)
+alert("bienvenido " + nombre + "!!!")
+let seguimos = true
+let año = 2022
+let edad = prompt("le pido porfavor " + nombre + " me escriba en que año nacio ,  19_ _?")
+while(seguimos) {
+    console.log("su edad es de ", edad - año )
+    seguimos = alert("su edad es" + (año - edad) )
+if (seguimos  ) {
+    edad++  } 
+else { (seguimos <= 18)
+    alert("Perfecto muchas gracias " + nombre + "!!!")
+    }
+}
+
+console.log(" un credito de cuanto quiere conseguir ?maximo es $300,000", nombre)
 let prestamo = parseInt(prompt(nombre + ", usted un credito de cuanto $$$ quiere conseguir ?maximo es $300,000"))
-
-
 if (prestamo < 1000) {
     console.error("no alcanza ,pide  mas!")
     alert(nombre + " no alcanza ,pide  mas!")
@@ -11,7 +22,6 @@ if (prestamo < 1000) {
 else if (prestamo < 10000) {
     console.warn("vamos yo se que quieres  mas ")
     alert(nombre + " vamos yo se que quieres  mas ")
-
 }
 else if (prestamo >= 30000) {
     console.log("puess tiene buena pinta, trato hecho!!!")
@@ -25,37 +35,28 @@ else {
 let interez = 1.43
 let ope = "+"
 let cuotas = parseInt(prompt("En  cuanta cantidad de cuotas lo va a querer ?"))
-    console.log("su plan de pago por mes es", calculo(prestamo, interez, cuotas, ope))
+console.log("su plan de pago por mes es", calculo(prestamo, interez, cuotas, ope))
 
 function calculo(num1, num2, num3, ope) {
     switch (ope) {
-        case "+":
-            return num1 * num2 / num3
+    case "+":
+    return num1 * num2 / num3
 
     }
 }
 
-
 confirm(nombre + "las cuotas por mes seran " + calculo(prestamo, interez, cuotas, ope))
-
-
 let masGrande = prompt("si usted requiere un credito mayor a  $500,000, por favor diga Si, de lo contrario ,  no")
 if (masGrande == "si") {
     console.log("Perfecto! digame el monto mayor!")
     alert("Perfecto! digame el monto mayor!")
-
 }
 else {
     console.warn("no se otorga el credito")
     alert("no se otorga el credito " + nombre)
-
-
 }
+
 let crediMayor = parseInt(prompt(nombre + " un credito de cuanto $$$ quiere conseguir ?maximo es $3,000,000"))
-
 let masCuotas = parseInt(prompt("En  cuanta cantidad de cuotas lo va a querer ?"))
-    console.log("su plan de pago por mes es", calculo(crediMayor, interez, masCuotas, ope))
-    confirm("su plan de pago por mes es" + calculo(crediMayor, interez, masCuotas, ope))
-
-
-
+console.log("su plan de pago por mes es", calculo(crediMayor, interez, masCuotas, ope))
+confirm("su plan de pago por mes es" + calculo(crediMayor, interez, masCuotas, ope))
